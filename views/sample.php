61d3539b15aea7968bb46ad03dad203c0cc34a62
shayiii0714 <shainamendoza0714@gmail.com>
2023-06-05 13:18:33 +0800

    Partial (Homepage)
    - Dashboard

--- views/sample.php
+++ views/sample.php
@@ -2,60 +2,72 @@
 <html>
 <head>
   <title>Sidebar Example</title>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-      font-family: Arial, sans-serif;
-    }
-
-    #sidebar {
-      width: 200px;
-      height: 100vh;
-      background-color: #090C1B;
-      color: #fff;
-      padding: 20px;
-      position: fixed;
-      left: 0;
-      top: 0;
-      overflow-y: auto;
-    }
-
-    #content {
-      margin-left: 200px;
-      padding: 20px;
-    }
-
-    h1 {
-      color: #333;
-    }
-
-    @media only screen and (max-width: 768px) {
-      #sidebar {
-        width: 100%;
-        height: auto;
-        position: static;
-      }
-
-      #content {
-        margin: 20px;
-      }
-    }
-  </style>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+  <link rel="stylesheet" href="..\stylesheet\sample.css" type="text/css">
+  <link rel="stylesheet" href="..\stylesheet\flash_messages.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="..\flash_messages.js"></script>
 </head>
 <body>
-  <div id="sidebar">
-    <h1>Sidebar</h1>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
+<div id="wrapper">
+
+<!-- Sidebar -->
+<div class="row">
+  <div class="col-3">
+    <div id="sidebar-wrapper">
+      <ul class="sidebar-nav text-white">
+        <a href="#">
+            <img src="../assets/images/logo-dashboard.png" alt="">
+        </a>
+        <li class="text-uppercase text-white text-center fs-5 mt-3 mb-3">
+          operator
+        </li>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+        <li>
+            <a href="#">Tricycles</a>
+        </li>
+        <li>
+            <a href="#">Drivers</a>
+        </li>
+        <li>
+            <a href="#">Documents</a>
+        </li>
+        <li>
+            <a href="#">Appointment</a>
+        </li>
+        <li>
+            <a href="#">Maintenance Log</a>
+        </li>
+        <li>
+            <a href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
   </div>
-  <div id="content">
-    <h1>Content Area</h1>
-    <p>This is the main content of the page.</p>
+  <div class="col-9">
+      <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                  <div class="title-head text-uppercase">
+                    <h4>Tricycles</h4>
+                  </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
   </div>
+</div>
 </body>
 </html>
