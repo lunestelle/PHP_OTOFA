d52bdf2abe13564dfcad93ecaf385e0960efc888
shayiii0714 <shainamendoza0714@gmail.com>
2023-06-06 21:38:13 +0800

    Icons & Responsiveness
    - Fix responsiveness from 1024px upto --
    - Icons

--- views/sample.php
+++ views/sample.php
@@ -16,141 +16,133 @@
 <div id="wrapper">
 
 <!-- Sidebar -->
-<div class="row">
-  <div class="col-md-2">
-    <div id="sidebar-wrapper">
-      <ul class="sidebar-nav text-white">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xl-2 col-lg-3 side-bar">
+      <div class="sidebar-nav text-white">
         <a href="..\views\index.php">
             <img src="../assets/images/logo-dashboard.png" alt="">
         </a>
-        <li class="text-uppercase text-white text-center fs-5 mt-3 mb-3">
-          operator
-        </li>
-        <li>
-          <a href="#"><i class="fa-solid fa-list"></i>Dashboard</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa-solid fa-truck-pickup"></i>Tricycles</a>
-        </li>
-        <li>
-            <a href="#"><i class="fa-regular fa-id-card"></i>Drivers</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa-solid fa-folder"></i>Documents</a>
-        </li>
-        <li>
-          <a href="#"> <i class="fa-solid fa-calendar-days"></i>Appointment</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa-solid fa-screwdriver-wrench"></i>Maintenance Log</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
-        </li>
-      </ul>
+        <div class="mt-5">
+          <li>
+            <a href="#"><i class="fa-solid fa-list mr-2"></i>Dashboard</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-truck-pickup mr-2"></i>Tricycles</a>
+          </li>
+          <li>
+              <a href="#"><i class="fa-regular fa-id-card mr-2"></i>Drivers</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-folder mr-2"></i>Documents</a>
+          </li>
+          <li>
+            <a href="#"> <i class="fa-solid fa-calendar-days mr-2"></i>Appointment</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-screwdriver-wrench"></i>Maintenance Log</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+          </li>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="col-md-10">
-    <div class="page-content-wrapper">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="title-head text-uppercase">
-              <h6>Dashboard</h6>
-            </div>
-            <div class="mt-3">
-                <a href="" class="text-uppercase register">Register</a>
-            </div>
-            <div class="container table-responsive pt-4"> 
-              <table class="table table-bordered table-hover">
-                <thead class="thead-custom">
-                  <tr class="text-center text-uppercase">
-                    <th scope="col">#</th>
-                    <th scope="col">Plate No.</th>
-                    <th scope="col">Color Code</th>
-                    <th scope="col">Driver's Name</th>
-                    <th scope="col">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>123</td>
-                    <td>Red</td>
-                    <td>Juan Dela Cruz</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>456</td>
-                    <td>Green</td>
-                    <td>Pedro Kalungsod</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>789</td>
-                    <td>Yellow</td>
-                    <td>Mario Bugsay</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>012</td>
-                    <td>Blue</td>
-                    <td>Lito Gaspi</td>
-                    <td>Actions</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="title-head text-uppercase mt-3">
-              <h6>Renewal of tricycle permits</h6>
-            </div>
-             <div class="container table-responsive pt-4"> 
-              <table class="table table-bordered table-hover">
-                <thead class="thead-custom">
-                  <tr class="text-center text-uppercase">
-                    <th scope="col">#</th>
-                    <th scope="col">Plate No.</th>
-                    <th scope="col">Color Code</th>
-                    <th scope="col">Driver's Name</th>
-                    <th scope="col">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>123</td>
-                    <td>Red</td>
-                    <td>Juan Dela Cruz</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>456</td>
-                    <td>Green</td>
-                    <td>Pedro Kalungsod</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>789</td>
-                    <td>Yellow</td>
-                    <td>Mario Bugsay</td>
-                    <td>Actions</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>012</td>
-                    <td>Blue</td>
-                    <td>Lito Gaspi</td>
-                    <td>Actions</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <div class="col-xl-10 col-lg-9 p-2">
+      <div class="title-head text-uppercase">
+        <h6>Dashboard</h6>
+      </div>
+      <div class="mt-3">
+          <a href="" class="text-uppercase register">Register</a>
+      </div>
+        <div class="container table-responsive pt-4"> 
+          <table class="table table-bordered table-hover">
+            <thead class="thead-custom">
+              <tr class="text-center text-uppercase">
+                <th scope="col">#</th>
+                <th scope="col">Plate No.</th>
+                <th scope="col">Color Code</th>
+                <th scope="col">Driver's Name</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>123</td>
+                <td>Red</td>
+                <td>Juan Dela Cruz</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>456</td>
+                <td>Green</td>
+                <td>Pedro Kalungsod</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>789</td>
+                <td>Yellow</td>
+                <td>Mario Bugsay</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>012</td>
+                <td>Blue</td>
+                <td>Lito Gaspi</td>
+                <td>Actions</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="title-head text-uppercase mt-3">
+          <h6>Renewal of tricycle permits</h6>
+        </div>
+          <div class="container table-responsive pt-4"> 
+          <table class="table table-bordered table-hover">
+            <thead class="thead-custom">
+              <tr class="text-center text-uppercase">
+                <th scope="col">#</th>
+                <th scope="col">Plate No.</th>
+                <th scope="col">Color Code</th>
+                <th scope="col">Driver's Name</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>123</td>
+                <td>Red</td>
+                <td>Juan Dela Cruz</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>456</td>
+                <td>Green</td>
+                <td>Pedro Kalungsod</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>789</td>
+                <td>Yellow</td>
+                <td>Mario Bugsay</td>
+                <td>Actions</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>012</td>
+                <td>Blue</td>
+                <td>Lito Gaspi</td>
+                <td>Actions</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
