dfc079169cb586e9ed450a9f84ecd56d7d698090
shayiii0714 <shainamendoza0714@gmail.com>
2023-06-08 14:29:19 +0800

    Dropdown
    - add dropdown

--- views/sample.php
+++ views/sample.php
@@ -5,7 +5,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="..\stylesheet\sample.css" type="text/css">
   <link rel="stylesheet" href="..\stylesheet\flash_messages.css">
@@ -25,27 +24,34 @@
         </a>
         <div class="mt-5">
           <li>
-            <a href="#"><i class="fa-solid fa-list mr-2"></i>Dashboard</a>
+            <a href="#"><i class="fa-solid fa-list"></i>Dashboard</a>
           </li>
           <li>
-            <a href="#"><i class="fa-solid fa-truck-pickup mr-2"></i>Tricycles</a>
+            <a href="#"><i class="fa-solid fa-truck-pickup"></i>Tricycles</a>
           </li>
           <li>
-              <a href="#"><i class="fa-regular fa-id-card mr-2"></i>Drivers</a>
+              <a href="#"><i class="fa-regular fa-id-card"></i>Drivers</a>
           </li>
           <li>
-            <a href="#"><i class="fa-solid fa-folder mr-2"></i>Documents</a>
+            <a href="#"><i class="fa-solid fa-folder"></i>Documents</a>
           </li>
           <li>
-            <a href="#"> <i class="fa-solid fa-calendar-days mr-2"></i>Appointment</a>
+            <a href="#"> <i class="fa-solid fa-calendar-days"></i>Appointment</a>
           </li>
           <li>
             <a href="#"><i class="fa-solid fa-screwdriver-wrench"></i>Maintenance Log</a>
-          </li>
-          <li>
-            <a href="#"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
-          </li>
+          </li> 
         </div>
+      </div>
+      <div class="dropdown">
+        <button class="btn color dropdown-toggle text-uppercase" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+          Juan &nbsp;
+          <i class="fa-solid fa-up-right-from-square"></i>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear"></i>Account</a></li>
+          <li><a class="dropdown-item" href="..\views\sign_in.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></li>
+        </ul>
       </div>
     </div>
     <div class="col-xl-10 col-lg-9 p-2">
