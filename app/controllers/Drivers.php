de401b12319a90920bb91379500ce120b5568b65
lunestelle <annalimpangog@gmail.com>
2023-11-26 18:12:31 +0800

    Update Drivers.php

--- app/controllers/Drivers.php
+++ app/controllers/Drivers.php
@@ -5,32 +5,45 @@ class Drivers
 	use Controller;
 
 	public function index()
-	{
-		if (!is_authenticated()) {
-			set_flash_message("Oops! You need to be logged <br> in to view this page.", "error");
-			redirect('');
-		}
+{
+  if (!is_authenticated()) {
+    set_flash_message("Oops! You need to be logged <br> in to view this page.", "error");
+    redirect('');
+  }
 
-		$driverModel = new Driver();
-    $driversData = $driverModel->findAll();
+  $driverModel = new Driver();
+  $driversData = $driverModel->where(['user_id' => $_SESSION['USER']->user_id]);
 
-    $data['drivers'] = [];
-		$data['index'] = 1;
+  $tricycleModel = new Tricycle();
+  $tricyclesData = $tricycleModel->findAll();
 
-		if (!empty($driversData)) {
-      foreach ($driversData as $driver) {
-        $data['drivers'][] = [
-          'driver_id' => $driver->driver_id,
-          'name' => $driver->first_name . ' ' . $driver->middle_name . ' ' . $driver->last_name,
-          'birthdate' => $driver->birth_date,
-          'address' => $driver->address,
-          'phone_no' => $driver->phone_no,
-          'license_no' => $driver->license_no,
-          'license_validity' => $driver->license_validity
-        ];
+  $data['drivers'] = [];
+  $data['index'] = 1;
+
+  if (!empty($driversData)) {
+    foreach ($driversData as $driver) {
+      $tricyclePlateNo = '';
+
+      foreach ($tricyclesData as $tricycle) {
+        if ($driver->tricycle_id === $tricycle->tricycle_id) {
+          $tricyclePlateNo = $tricycle->plate_no;
+          break;
+        }
       }
-    }
 
-		echo $this->renderView('drivers', true, $data);
-	}
+      $data['drivers'][] = [
+        'driver_id' => $driver->driver_id,
+        'name' => $driver->first_name . ' ' . $driver->middle_name . ' ' . $driver->last_name,
+        'birthdate' => $driver->birth_date,
+        'address' => $driver->address,
+        'phone_no' => $driver->phone_no,
+        'license_no' => $driver->license_no,
+        'license_validity' => $driver->license_validity,
+        'tricycle_plate_number' => $tricyclePlateNo,
+      ];
+    }
+  }
+
+    echo $this->renderView('drivers', true, $data);
+  }
 }
