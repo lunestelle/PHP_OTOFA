80e075030d875ad1d4510fb6005edb632cc774fc
shayiii0714 <102460202+shayiii0714@users.noreply.github.com>
2023-12-02 23:23:15 +0800

    centered-images

--- app/views/view_tricycle.php
+++ app/views/view_tricycle.php
@@ -112,7 +112,7 @@
                   <div class="modal-header border-0">
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  <div class="modal-body">
+                  <div class="modal-body mx-auto">
                     <img src="" class="img-fluid" id="modalImage" alt="Enlarged Image">
                   </div>
                 </div>
