9ebe442cf4ba9e8d5648ba91a9e75b81b2902b36
lunestelle <annalimpangog@gmail.com>
2023-12-04 05:25:40 +0800

    Update active_links.js
    - Added active links to the added features

--- public/assets/js/active_links.js
+++ public/assets/js/active_links.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         $(this).removeClass('nav-link-active');
       }
     } else {
-      if (currentUrl.href.includes('new_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('edit_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('view_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('new_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('view_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('edit_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('new_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('view_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('edit_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('new_taripa') && linkUrl.includes('taripa') || currentUrl.href.endsWith(linkUrl) || currentUrl.href.includes('view_operator') && linkUrl.includes('operators')) {
+      if (currentUrl.href.includes('new_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('edit_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('view_tricycle') && linkUrl.includes('tricycles') || currentUrl.href.includes('new_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('view_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('edit_driver') && linkUrl.includes('drivers') || currentUrl.href.includes('new_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('view_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('edit_appointment') && linkUrl.includes('appointment') || currentUrl.href.includes('new_taripa') && linkUrl.includes('taripa') || currentUrl.href.includes('new_maintenance_log') && linkUrl.includes('maintenance_logs') || currentUrl.href.includes('edit_maintenance_log') && linkUrl.includes('maintenance_logs') || currentUrl.href.includes('view_maintenance_log') && linkUrl.includes('maintenance_logs') || currentUrl.href.endsWith(linkUrl) || currentUrl.href.includes('view_operator') && linkUrl.includes('operators')) {
         $(this).addClass('nav-link-active');
       } else {
         $(this).removeClass('nav-link-active');
@@ -31,6 +31,12 @@ $(document).ready(function() {
     return;
   } else if (currentUrl.href.includes('green_trike_info')) {
     $('.sidebar a[href="dashboard"]').addClass('nav-link-active');
+    return;
+  } else if (currentUrl.href.includes('tricycles?status=active')) {
+    $('.sidebar a[href="tricycles"]').addClass('nav-link-active');
+    return;
+  } else if (currentUrl.href.includes('appointments?status=pending')) {
+    $('.sidebar a[href="appointments"]').addClass('nav-link-active');
     return;
   }
 });
