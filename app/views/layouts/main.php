233c89f3ecb342c4a00ac3da69abcf49f197d5b4
lunestelle <annalimpangog@gmail.com>
2024-01-28 18:01:25 +0800

    Update the favicon

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -50,8 +50,8 @@ $usedCINs = $tricycleModel->where(['is_used' => true]);
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>SAKAYCLE | A Web-based Management System for Tricycle Franchise Appointment</title>
-  <link rel="icon" href="public/assets/images/logo.png" type="image/x-icon">
+  <title>OTOFA | Ormoc Tricycle Online Franchise Appointment</title>
+  <link rel="icon" href="public/assets/images/logo-dashboard.png" type="image/x-icon">
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
