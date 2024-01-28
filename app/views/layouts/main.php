598f4ae168e398fee275d9fce120ae85c2042e8c
lunestelle <annalimpangog@gmail.com>
2024-01-28 19:59:33 +0800

    Updated favicon used

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -51,7 +51,7 @@ $usedCINs = $tricycleModel->where(['is_used' => true]);
 <head>
   <meta charset="UTF-8">
   <title>OTOFA | Ormoc Tricycle Online Franchise Appointment</title>
-  <link rel="icon" href="public/assets/images/logo-dashboard.png" type="image/x-icon">
+  <link rel="icon" href="public/assets/images/icon-logo.png" type="image/x-icon">
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
